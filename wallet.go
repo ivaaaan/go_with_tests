d00e826cd62eb63b8a2fdc5bd6fb7d61dbda4d97
Ivan Klymenchenko <iillexial@gmail.com>
2019-10-16 22:29:04 +0300

    Points & errors

---
+++ wallet.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
+
+func (w Wallet) Balance() Bitcoin {
+	return w.balance
+}
