a4aaa65a85f12aa7e3f11964e61715975d1b6a59
Ivan Klymenchenko <iillexial@gmail.com>
2019-10-19 19:16:03 +0300

    DI, Mocks

---
+++ countdown.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprint(out, finalWord)
+}
